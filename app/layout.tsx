99a0d24f5dce69fdfad7a226beef81f3479ddfe6
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-09-09 14:52:34 -0500

    Re estructura proyecto

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
 
 import { Metadata } from "next";
+import SideNav from "./ui/dashboard/sidenav";
 
 export const metadata: Metadata = {
   title: {
@@ -19,7 +20,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div
+          className="flex h-screen flex-col md:flex-row md:overflow-hidden"
+          style={{ maxWidth: "1200px", margin: '0 auto' }}
+        >
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
