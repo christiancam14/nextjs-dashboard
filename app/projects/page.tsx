177c1e6c52f3398fc6c6f381b06d17a2bfd43fc4
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-09-10 18:21:52 -0500

    Fix projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,8 +4,11 @@ export const metadata: Metadata = {
   title: "Projects",
 };
 
-export const ProjectsPage = () => {
-  return <div>Projects page</div>;
-};
-
-export default ProjectsPage;
+export default function ProjectsPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page</p>
+    </div>
+  );
+}
